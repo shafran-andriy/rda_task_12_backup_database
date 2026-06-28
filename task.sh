cb60cf002d14685f8c7cc420722676fbc945eb7b
shafran <shafran.andriy@gmail.com>
2026-06-28 12:18:46 +0300

    fix7

--- task.sh
+++ task.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # Create full backup the database ShopDB
-mysqldump -u $DB_USER -p$DB_PASSWORD ShopDB --result-file=ShopDB-Full.sql --no-create-db
+mysqldump -u $DB_USER -p$DB_PASSWORD --databases ShopDB --result-file=ShopDB-Full.sql --no-create-db
 
 
 # Check if the source database directory exists
@@ -16,9 +16,10 @@ fi
 
 
 # Check if the source database directory exists
-if [ -d "/var/lib/mysql/ShopDB" ]; then
-    # Copy with overwrite (-r for recursive, -f for force overwrite)
-    cp -r /var/lib/mysql/ShopDB /var/lib/mysql/ShopDBDevelopment
+if [ -d "/var/lib/mysql/ShopDBDevelopment" ]; then
+    mysqldump -u $DB_USER -p$DB_PASSWORD ShopDB --no-create-info --no-create-db > ShopDB-Data.sql
+    mysql -u $DB_USER -p$DB_PASSWORD ShopDBDevelopment < ShopDB-Data.sql
+
 else
     echo "Database directory /var/lib/mysql/ShopDB does not exist."
 fi
