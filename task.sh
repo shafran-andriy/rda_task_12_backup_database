b3ea4cf596de8a8c21e608d6ebb9bb6a42736dc2
shafran <shafran.andriy@gmail.com>
2026-06-28 12:28:36 +0300

    fix9

--- task.sh
+++ task.sh
@@ -1,25 +1,7 @@
-#! /bin/bash
+#!/bin/bash
 
-# Create full backup the database ShopDB
-mysqldump -u $DB_USER -p$DB_PASSWORD --databases ShopDB --result-file=ShopDB-Full.sql --no-create-db
+# Full backup of ShopDB (schema + data), restore to ShopDBReserve
+mysqldump -u "$DB_USER" -p"$DB_PASSWORD" ShopDB | mysql -u "$DB_USER" -p"$DB_PASSWORD" ShopDBReserve
 
-
-# Check if the source database directory exists
-if [ -d "/var/lib/mysql/ShopDBReserve" ]; then
-    # Restore full backup the database ShopDB in the database ShopDBReserve
-    mysql -u $DB_USER -p$DB_PASSWORD ShopDBReserve < ShopDB-Full.sql
-else
-    mysql -u root -p -e "CREATE DATABASE ShopDBReserve;"
-    sleep 5
-    mysql -u $DB_USER -p$DB_PASSWORD ShopDBReserve < ShopDB-Full.sql
-fi
-
-
-# Check if the source database directory exists
-if [ -d "/var/lib/mysql/ShopDBDevelopment" ]; then
-    mysqldump -u $DB_USER -p$DB_PASSWORD ShopDB --no-create-info --no-create-db > ShopDB-Data.sql
-    mysql -u $DB_USER -p$DB_PASSWORD ShopDBDevelopment < ShopDB-Data.sql
-
-else
-    echo "Database directory /var/lib/mysql/ShopDB does not exist."
-fi
+# Data-only backup of ShopDB (no schema), restore to ShopDBDevelopment
+mysqldump -u "$DB_USER" -p"$DB_PASSWORD" --no-create-info ShopDB | mysql -u "$DB_USER" -p"$DB_PASSWORD" ShopDBDevelopment
