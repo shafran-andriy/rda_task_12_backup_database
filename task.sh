dea8bebec471a17b34683cf6c92e005e034f84b7
shafran <shafran.andriy@gmail.com>
2026-06-28 12:35:31 +0300

    fix

--- task.sh
+++ task.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Full backup of ShopDB (schema + data), restore to ShopDBReserve
-mysqldump -u "$DB_USER" -p"$DB_PASSWORD" ShopDB | mysql -u "$DB_USER" -p"$DB_PASSWORD" ShopDBReserve
+mysqldump -u "$DB_USER" -p"$DB_PASSWORD" ShopDB --result-file=ShopDBFull.sql
+mysql -u "$DB_USER" -p"$DB_PASSWORD" ShopDBReserve > ShopDBFull.sql
 
 # Data-only backup of ShopDB (no schema), restore to ShopDBDevelopment
-mysqldump -u "$DB_USER" -p"$DB_PASSWORD" --no-create-info ShopDB | mysql -u "$DB_USER" -p"$DB_PASSWORD" ShopDBDevelopment
+mysqldump -u "$DB_USER" -p"$DB_PASSWORD" --no-create-info ShopDB --result-file=ShopDB-NO-Schema.sql
+mysql -u "$DB_USER" -p"$DB_PASSWORD" ShopDBDevelopment > ShopDB-NO-Schema.sql
